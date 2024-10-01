9b1c68103115175fff23c2fa14eb6c7b2a0a387b
Sai Medhini Reddy Maryada <saimedhinireddymaryada@Sais-MacBook-Pro.local>
2024-09-30 21:31:56 -0700

    added2

--- cypress/integration/plugins/dashboards-flow-framework/workflows.js
+++ cypress/integration/plugins/dashboards-flow-framework/workflows.js
@@ -5,6 +5,7 @@
 
 import {
     FF_URL,
+    FF_FIXTURE_BASE_PATH,
   } from '../../../utils/constants';
 
   
@@ -20,6 +21,15 @@ import {
     it('create workflow using import', () => {
         cy.getElementByTestId('importWorkflowButton').should('be.visible').click();
         cy.contains('Import a workflow (JSON/YAML)').should('be.visible');
+        const fileName =  FF_FIXTURE_BASE_PATH + 'sample-workflow.json';
+        cy.get('input[type="file"]').attachFile(fileName);
+        cy.get('button:contains("Import")').should('not.be.disabled').click();
+      });
+
+      it('create workflow using template', () => {
+        cy.contains('h2', 'Custom').parents('.euiCard').within(() => {
+            cy.contains('button', 'Go').click();
+          });
       });
   
   })
